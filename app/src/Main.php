ca5519b7bd6ce5612c5ce8cd282870c16df6c77d
Sam Heuck <sam@samheuck.com>
2015-08-13 23:18:57 -0500

    adds login form
    Signed-off-by: Sam Heuck <sam@samheuck.com>

--- app/src/Main.php
+++ app/src/Main.php
@@ -8,8 +8,16 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Main
 {
-    public function index(Request $request, Application $app)
+    public function index(Request $req, Application $app)
     {
         return $app->render('@app/index.twig');
+    }
+
+    public function login(Request $req, Application $app)
+    {
+        return $app->render('@app/login.twig', [
+            'error' => $app['security.last_error']($req),
+            'last_username' => $app['session']->get('_security.last_username'),
+        ]);
     }
 }
